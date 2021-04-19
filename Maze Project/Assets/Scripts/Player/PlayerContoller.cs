934a7002b1b7c4ec6d1ee5ef56acced942f23895
K_Arai00 <akgame36@gmail.com>
2021-04-19 19:04:56 +0900

    フォルダを作って移動＆スクリプト追加

---
+++ Maze Project/Assets/Scripts/Player/PlayerContoller.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary> Playerを制御するクラス </summary>
+[RequireComponent(typeof(CapsuleCollider))]
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerContoller : MonoBehaviour
+{
+    
+}
