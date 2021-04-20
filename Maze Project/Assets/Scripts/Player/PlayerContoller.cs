498d4afab52488eb0c9d30b455bd3a3d9b3d9504
K_Arai00 <akgame36@gmail.com>
2021-04-20 13:35:19 +0900

    プレイヤーコントローラーとりあえずできた。

--- Maze Project/Assets/Scripts/Player/PlayerContoller.cs
+++ Maze Project/Assets/Scripts/Player/PlayerContoller.cs
@@ -8,5 +8,38 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerContoller : MonoBehaviour
 {
-    
+    /// <summary> 参照しているクラスの変数 </summary>
+    [SerializeField] DynamicJoystick _DynamicJoystick = null;
+    /// <summary> アニメーターの変数 </summary>
+    //[SerializeField] Animator _Anim = null;
+    /// <summary> プレイヤーのスピード </summary>
+    [SerializeField] float _Speed = 1f;
+    /// <summary> 参照しているクラスの変数 </summary>
+    private CharacterController _CharacterController = null;
+    private Vector3 _Direction;
+    private Rigidbody _Rb = null;
+
+    private void Start()
+    {
+        _CharacterController = GetComponent<CharacterController>();
+        _Rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        //
+        if (_Direction != new Vector3(0, 0, 0))
+        {
+            transform.localRotation = Quaternion.LookRotation(_Direction);
+        }
+
+        _CharacterController.Move(_Direction * _Speed * Time.deltaTime);
+    }
+
+    public void FixedUpdate()
+    {
+        //
+        _Direction = Vector3.forward * _DynamicJoystick.Vertical + Vector3.right * _DynamicJoystick.Horizontal;
+        _Rb.AddForce(_Direction * _Speed * Time.fixedDeltaTime, ForceMode.VelocityChange);
+    }
 }
