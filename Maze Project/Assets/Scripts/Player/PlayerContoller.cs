435804283e7d53f12590aae6ab2f69931725ef0b
K_Arai00 <akgame36@gmail.com>
2021-04-20 10:55:42 +0900

    追加

--- Maze Project/Assets/Scripts/Player/PlayerContoller.cs
+++ Maze Project/Assets/Scripts/Player/PlayerContoller.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary> Playerを制御するクラス </summary>
+[RequireComponent(typeof(CharacterController))]
 [RequireComponent(typeof(CapsuleCollider))]
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerContoller : MonoBehaviour
