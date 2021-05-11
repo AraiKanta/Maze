66de36b3a8c7fe8f12324e124e8eaa8ea301d1fd
K_Arai00 <akgame36@gmail.com>
2021-05-11 20:22:47 +0900

    書いた

---
+++ Maze Project/Assets/Scripts/UI/Credit.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary> Creditを表示するクラス </summary>
+public class Credit : MonoBehaviour
+{
+    [Header("テキストのスクロールスピード")]
+    [SerializeField] private float textScrollSpeed = 30;
+    [Header("テキストの制限位置")]
+    [SerializeField] private float limitPosition = 730f;
+    /// <summary>エンドロールが終了したかどうか </summary>
+    private bool isStopStaffRoll;
+    /// <summary>シーン移動用コルーチン </summary>
+    private Coroutine _staffRollCoroutine;
+
+    void Update()
+    {
+        // エンドロールが終了した時
+        if (isStopStaffRoll)
+        {
+            _staffRollCoroutine = StartCoroutine(GoToNextScene());
+        }
+        else
+        {
+            // エンドロール用テキストがリミットを越えるまで動かす
+            if (transform.position.y <= limitPosition)
+            {
+                transform.position = new Vector2(transform.position.x, transform.position.y + textScrollSpeed * Time.deltaTime);
+            }
+            else
+            {
+                isStopStaffRoll = true;
+            }
+        }
+    }
+
+    IEnumerator GoToNextScene()
+    {
+        // 5秒間待つ
+        yield return new WaitForSeconds(2f);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            StopCoroutine(_staffRollCoroutine);
+        }
+
+        yield return null;
+    }
+}
