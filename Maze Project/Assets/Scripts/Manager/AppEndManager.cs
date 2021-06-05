bed89bc8f411854453e7acea93f12414092cb26e
K_Arai00 <akgame36@gmail.com>
2021-06-05 20:16:14 +0900

    アプリケーションを終了させるクラスの追加

---
+++ Maze Project/Assets/Scripts/Manager/AppEndManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+/// <summary> アプリケーションを終了させるクラス </summary>
+public class AppEndManager : MonoBehaviour
+{
+    /// <summary>
+    /// 終了ボタン
+    /// </summary>
+    [System.Obsolete]
+    private void OnApplicationQuit()
+    {
+        // 終了処理をキャンセル
+        Application.CancelQuit();
+    }
+
+    /// <summary>
+    /// 終了
+    /// </summary>
+    public void OnExit()
+    {
+        Application.Quit();
+    }
+}
