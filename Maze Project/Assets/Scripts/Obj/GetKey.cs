2f2277fd3f04e2c9b7722394856eaa8dc8d6c0b1
K_Arai00 <akgame36@gmail.com>
2021-07-08 19:09:06 +0900

    変更有

--- Maze Project/Assets/Scripts/Obj/GetKey.cs
+++ Maze Project/Assets/Scripts/Obj/GetKey.cs
@@ -3,14 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+// TODO:実装途中
 /// <summary> 鍵を取ったときゴールを出現させるクラス </summary>
 public class GetKey : MonoBehaviour
 {
     /// <summary> 鍵オブジェクトの変数 </summary>
-    private GameObject m_key = null;
+    private GameObject m_key;
     /// <summary> ゴールの座標 </summary>
     [Header("ゴールの座標(GoalObjPrefabが入る)")]
     [SerializeField] Transform m_goalObj;
+
+    private void Start()
+    {
+        //m_goalObj.gameObject.SetActive(false);
+        m_goalObj = GameObject.FindGameObjectWithTag("GoalObj").transform;
+    }
 
     private void OnTriggerEnter(Collider collider)
     {
