49dca82782168774d6a8516cf1e849c28cad21c2
K_Arai00 <akgame36@gmail.com>
2021-07-13 20:29:39 +0900

    難易度によってサイズの値を渡すクラスの追加

---
+++ Maze Project/Assets/Scripts/Manager/DifficultyManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DifficultyManager : MonoBehaviour
+{
+
+    private int size = default; 
+
+
+    public void Difficulty(Scene next, LoadSceneMode mode) 
+    {
+        GameObject.Find("MazeGanerator").GetComponent<MazeGanerator>().setMazeSize = size;
+
+        SceneManager.sceneLoaded -= Difficulty;
+    }
+
+    /// <summary>
+    /// ボタンが押されたときにマップサイズを変更するメソッドEasy() 
+    /// </summary>
+    public void Easy() 
+    {
+        size = 7;
+    }
+    /// <summary>
+    /// ボタンが押されたときにマップサイズを変更するメソッドNomal()
+    /// </summary>
+    public void Nomal()
+    {
+        size = 15;
+    }
+    /// <summary>
+    /// ボタンが押されたときにマップサイズを変更するメソッドHeard()
+    /// </summary>
+    public void Heard()
+    {
+        size = 21;
+    }
+}
