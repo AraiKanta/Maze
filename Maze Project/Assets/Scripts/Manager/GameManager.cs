44a08b6e6881be42b5bf913fea1b15962bf42cc4
K_Arai00 <akgame36@gmail.com>
2021-07-27 16:39:23 +0900

    ゲームオーバー時と鍵をとったときの処理の追加

--- Maze Project/Assets/Scripts/Manager/GameManager.cs
+++ Maze Project/Assets/Scripts/Manager/GameManager.cs
@@ -7,21 +7,33 @@ using UnityEngine.SceneManagement;
 /// <summary> ゲームの進行を管理するクラス </summary>
 public class GameManager : MonoBehaviour
 {
-    /// <summary> ゲームの状態 </summary>
-    GameState m_gameState = GameState.NonInitialized;
     /// <summary> ゴール後に表示させるキャンバス </summary>
     [Header("ゴール後に表示させるキャンバス")]
     [SerializeField] private GameObject m_goalCavas = null;
-    /// <summary> スタート前のCountdownするテキストの変数 </summary>
-    [Header("Countdownするテキスト")]
-    [SerializeField] private Text m_countText = null;
+    /// <summary> ゲームオーバー後に表示させるキャンバス </summary>
+    [Header("ゲームオーバー後に表示させるキャンバ")]
+    [SerializeField] private GameObject m_timeUpCavas = null;
     /// <summary> オーディオマネージャーを参照している変数 </summary>
     AudioManager m_audioManager;
     /// <summary> ゲームクリア時に一回だけBGMを鳴らすためのフラグ </summary>
     private bool m_isClear = false;
+    /// <summary> 鍵オブジェクトの変数 </summary>
+    [Header("鍵の座標(KeyObjPrefabが入る)")]
+    [SerializeField] private Transform m_keyObj = null;
+    /// <summary> 鍵を獲得した時に表示するテキスト </summary>
+    [Header("鍵を獲得した時に表示するテキスト")]
+    [SerializeField] Text m_getKeyText = null;
+    /// <summary> 遅延させる時間 </summary>
+    [Header("点滅の間隔の時間")]
+    [SerializeField] private float m_duration = default;
+    ///<summary> ゲームの状態 </summary>
+    [Header("現在のゲームの状態")]
+    public GameState m_gameState;
 
-    /// <summary> ゲームの状態 </summary>
-    enum GameState
+    /// <summary> 
+    /// ゲームの状態 
+    /// </summary>
+    public enum GameState
     {
         /// <summary> 初期化前 </summary>
         NonInitialized,
@@ -37,13 +49,18 @@ public class GameManager : MonoBehaviour
 
     private void Start()
     {
-        if (GameObject.FindObjectOfType<AudioManager>())
+        m_getKeyText.gameObject.SetActive(false);
+        Debug.Log("テキスト非表示");
+
+        m_gameState = GameState.NonInitialized;
+
+        if (FindObjectOfType<AudioManager>())
         {
-            m_audioManager = GameObject.FindObjectOfType<AudioManager>().GetComponent<AudioManager>();
+            m_audioManager = FindObjectOfType<AudioManager>().GetComponent<AudioManager>();
         }
     }
 
-    void Update()
+    private void Update()
     {
         switch (m_gameState)
         {
@@ -52,19 +69,22 @@ public class GameManager : MonoBehaviour
                 break;
             case GameState.Initialized:
                 m_gameState = GameState.InGame;
-                StartCoroutine(CountStart());
                 break;
             case GameState.InGame:
+                Debug.Log("InGame");
                 break;
             case GameState.Finished:
                 Finished();
                 break;
             case GameState.TimeUp:
+                TimeUP();
                 break;
         }
     }
 
-    /// <summary> ゴールの関数 </summary>
+    /// <summary> 
+    /// ゴールの関数 
+    /// </summary>
     public void Finished()
     {
         //ゴール後のUIを非アクティブからアクティブにする
@@ -81,30 +101,29 @@ public class GameManager : MonoBehaviour
             }  
         }
 
+        Debug.Log("GameClear");
+
         // ステートをゴールした状態に更新する
         m_gameState = GameState.Finished;
     }
 
+    /// <summary>
+    /// 制限時間によるゲームオーバー時のメソッド
+    /// </summary>
     public void TimeUP()
     {
+        //ゲームオーバー後のUIを非アクティブからアクティブにする
+        m_timeUpCavas.SetActive(true);
+
+        Debug.Log("TimeUp");
+
+        // ステートをTimeUPした状態に更新する
         m_gameState = GameState.TimeUp;
-    }
+    } 
 
-    IEnumerator CountStart() 
-    {
-        m_countText.text = "3";
-        yield return new WaitForSeconds(1f);
-        m_countText.text = "2";
-        yield return new WaitForSeconds(1f);
-        m_countText.text = "1";
-        yield return new WaitForSeconds(1f);
-        m_countText.text = "Start!";
-        yield return new WaitForSeconds(0.5f);
-        m_countText.gameObject.SetActive(false);
-        // ステートをゴールした状態に更新する
-        m_gameState = GameState.InGame;
-    }
-
+    /// <summary>
+    /// ClickしたときにTitleに戻る処理を行っているメソッド
+    /// </summary>
     public void OnClickTitle()
     {
         if (m_audioManager)
@@ -114,5 +133,28 @@ public class GameManager : MonoBehaviour
         }
         //タイトルにシーンを遷移させる
         SceneManager.LoadScene("Title");
+    }
+
+    /// <summary> 
+    /// 鍵の座標をセットしている 
+    /// </summary>
+    public void SetKey(Transform key)
+    {
+        m_keyObj = key;
+    }
+
+    /// <summary> 
+    /// 鍵を獲得したときにテキストを表示するメソッド 
+    /// </summary>
+    IEnumerator GetKeyText()
+    {
+        m_getKeyText.GetComponent<Text>();
+        m_getKeyText.gameObject.SetActive(true);
+        yield return new WaitForSeconds(3f);
+        m_getKeyText.gameObject.SetActive(false);
+    }
+    public void TextStartCoroutine()
+    {
+        StartCoroutine(GetKeyText());
     }
 }
