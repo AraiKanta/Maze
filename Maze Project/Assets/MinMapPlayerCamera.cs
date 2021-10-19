14033e5b350b981e853c4b33b8c347174b55b52e
K_Arai00 <akgame36@gmail.com>
2021-10-19 10:57:08 +0900

    ミニマップのプレイヤーカメラのクラスを作成

---
+++ Maze Project/Assets/MinMapPlayerCamera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary> ミニマップ用のプレイヤーCameraのクラス </summary>
+public class MinMapPlayerCamera : MonoBehaviour
+{
+    /// <summary> プレイヤーの座標 </summary>
+    [Header("プレイヤーの座標(PlayerPrefabを入れる)")]
+    [SerializeField] private Transform m_player;
+
+    /// <summary> プレイヤーの座標をセットしている </summary>
+    public void SetPlayer(Transform player)
+    {
+        m_player = player;
+    }
+
+
+}
